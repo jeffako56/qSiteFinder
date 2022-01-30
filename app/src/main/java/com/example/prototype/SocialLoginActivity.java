b33a1b26d6e4a972052bd037fe70bfe614944565
Jepoy <jeffako45@gmail.com>
2022-01-30 13:12:17 +0800

    added fblogin && glogin

--- app/src/main/java/com/example/prototype/SocialLoginActivity.java
+++ app/src/main/java/com/example/prototype/SocialLoginActivity.java
@@ -1,8 +1,13 @@
 package com.example.prototype;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -10,6 +15,7 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -19,35 +25,55 @@ import com.facebook.FacebookException;
 import com.facebook.login.Login;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class SocialLoginActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = "Check";
     private CallbackManager callbackManager;
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton signInButton;
+    LoginButton loginButton;
+    GoogleSignInAccount acct;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_login);
 
-//        printHashKey();
         CallbackManager callbackManager = CallbackManager.Factory.create();
 
-        Log.e("jeff",callbackManager.toString());
-//        Toast.makeText(getApplicationContext(), callbackManager.toString(), Toast.LENGTH_SHORT).show();
+        Log.e("jeff", callbackManager.toString());
 
         final String EMAIL = "email";
 
         callbackManager = CallbackManager.Factory.create();
 
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+
         loginButton.setReadPermissions("public_profile", "email", "user_friends");
+
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onError(@NonNull FacebookException e) {
-                Log.i("Error" , "Error");
+                Log.i("Error", "Error");
             }
 
             @Override
@@ -64,10 +90,73 @@ public class SocialLoginActivity extends AppCompatActivity {
             }
         });
 
+
+        //googlelogin
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestProfile()
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            Log.e("may nakalogin na", " wow");
+        } else {
+            Log.e("wala pa nakalogin", " wow");
+            signInButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.e("Pa gSignin", "wowowin");
+                    signIn();
+                }
+            });
+        }
+
+        checkgoogleifloggedin();
     }
 
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void checkgoogleifloggedin() {
+            acct = GoogleSignIn.getLastSignedInAccount(SocialLoginActivity.this);
+            if (acct != null) {
+                Intent i = new Intent(SocialLoginActivity.this, home_page.class);
+                startActivity(i);
+            }
+    }
 
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Intent i = new Intent(SocialLoginActivity.this, home_page.class);
+            startActivity(i);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+//            updateUI(null);
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
 }
+
--- app/src/main/java/com/example/prototype/home_page.java
+++ app/src/main/java/com/example/prototype/home_page.java
@@ -1,14 +1,169 @@
 package com.example.prototype;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class home_page extends AppCompatActivity {
+    private AccessTokenTracker accessTokenTracker;
+    private CallbackManager callbackManager;
+    private ProfileTracker profileTracker;
+    TextView name;
+    TextView email;
+    GoogleSignInClient mGoogleSignInClient;
+    Button logout;
+    Button glogout;
+    GoogleSignInAccount acct;
+    ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+
+        logout = (Button) findViewById(R.id.fblogout_button_homepage);
+        glogout = (Button) findViewById(R.id.glogout_button_homepage);
+        name = (TextView) findViewById(R.id.text_name);
+        email = (TextView) findViewById(R.id.text_email);
+
+        glogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        fblogout();
+        glogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                glogout();
+            }
+        });
+
+
+        acct = GoogleSignIn.getLastSignedInAccount(home_page.this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+            name.setText(personName + personGivenName + personFamilyName);
+            email.setText(personEmail);
+
+
+        } else {
+            glogout.setEnabled(false);
+        }
+
+        if (!isLoggedIn()) {
+            logout.setEnabled(false);
+        }
+
     }
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+
+
+
+    private void glogout() {
+        GoogleSignInClient mGoogleApiClient;
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestProfile()
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                        Intent i = new Intent(home_page.this, SocialLoginActivity.class);
+                        startActivity(i);
+                    }
+                });
+
+    }
+
+    private void fblogout() {
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progress = new ProgressDialog(home_page.this);
+                progress.setTitle("Loading");
+                progress.setMessage("Wait while loading...");
+                progress.setCancelable(false); // disable dismiss by tapping outside of the dialog
+                progress.show();
+                disconnectFromFacebook();
+            }
+        });
+    }
+
+
+    public void disconnectFromFacebook() {
+
+        if (AccessToken.getCurrentAccessToken() == null) {
+
+            return; // already logged out
+        }
+
+        new GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/", null, HttpMethod.DELETE, new GraphRequest
+                .Callback() {
+            @Override
+            public void onCompleted(GraphResponse graphResponse) {
+                Log.e("fb logged out", "wow");
+                progress.dismiss();
+                LoginManager.getInstance().logOut();
+                Intent i = new Intent(home_page.this, SocialLoginActivity.class);
+                startActivity(i);
+            }
+        }).executeAsync();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        profileTracker.stopTracking();
+    }
+
+
 }
